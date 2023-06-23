92ff87f565f172487867ff4512efb9202fb8a934
bryce-sncf <bryce.marcol@sncf.fr>
2023-06-23 10:34:05 +0200

    correction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NasaService } from './nasa.service';
+import { NasaService } from './service/nasa.service';
 
 @Component({
   selector: 'app-root',
